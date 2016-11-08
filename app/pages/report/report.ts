1c252cb83c8eb3407d04fc3414fe931c8a743c41
kolexinfos <kolexinfos@gmail.com>
2016-11-08 12:35:54 +0000

    Layout Report Page

--- app/pages/report/report.ts
+++ app/pages/report/report.ts
@@ -11,23 +11,13 @@ import {CustomValidators} from '../validators/CustomValidators';
 
 export class ReportPage {
 
-    authForm: ControlGroup;
-    username: AbstractControl;
-    password: AbstractControl;
+   
+    
+    report: {email?: string, message?: string, phone?: string,title?: string} = {};
 
     constructor(private navController: NavController, private fb: FormBuilder) {
-        this.authForm = fb.group({  
-            'username': ['', Validators.compose([Validators.required, Validators.minLength(8), CustomValidators.checkFirstCharacterValidator])],
-            'password': ['', Validators.compose([Validators.required, Validators.minLength(8), CustomValidators.checkFirstCharacterValidator])]
-        });
- 
-        this.username = this.authForm.controls['username'];     
-        this.password = this.authForm.controls['password'];  
+       
     }
 	
-    onSubmit(value: string): void { 
-        if(this.authForm.valid) {
-            console.log('Submitted value: ', value);
-        }
-    } 	
+   	
 }
